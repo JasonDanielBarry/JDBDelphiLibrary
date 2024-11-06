020e0bd26199d10f9a55ff2b241e19c11c6211fa
Jason Barry <jasonbarry@prokon.com>
2024-11-06 08:40:39 +0200

    (1) added some functions to ColourMethods.pas

--- Source/Graphics/ColourMethods.pas
+++ Source/Graphics/ColourMethods.pas
@@ -8,6 +8,10 @@ interface
     function colourToAlphaColour(   const colourIn  : TColor;
                                     const opacityIn : integer = 255) : TAlphaColor;
 
+    function makeAlphaColour(redIn, greenIn, blueIn, opacityIn : byte) : TAlphaColor; inline;
+
+    function makeColour(redIn, greenIn, blueIn : byte) : TColor;
+
 implementation
 
     function colourToAlphaColour(   const colourIn  : TColor;
@@ -23,6 +27,20 @@ implementation
             redGreenBlue.rgbtBlue   := GetBValue( colourReferenceHex );
 
             result := MakeColor( redGreenBlue.rgbtRed, redGreenBlue.rgbtGreen, redGreenBlue.rgbtBlue, opacityIn );
+        end;
+
+    function makeAlphaColour(redIn, greenIn, blueIn, opacityIn : byte) : TAlphaColor;
+        begin
+            result := MakeColor( redIn, greenIn, blueIn, opacityIn );
+        end;
+
+    function makeColour(redIn, greenIn, blueIn : byte) : TColor;
+        var
+            tempAlphaColour : TAlphaColor;
+        begin
+            tempAlphaColour := MakeColor(redIn, greenIn, blueIn, 255);
+
+            result := AlphaColorToColor( tempAlphaColour );
         end;
 
 end.
