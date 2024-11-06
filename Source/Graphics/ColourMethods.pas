559ed1aca62705acccf0fdc3847011cd0d262f2a
Jason Barry <jasonbarry@prokon.com>
2024-11-06 09:43:16 +0200

    (1) wrote functions to work with and convert between TColor and TAlphaColor

--- Source/Graphics/ColourMethods.pas
+++ Source/Graphics/ColourMethods.pas
@@ -5,42 +5,100 @@ interface
     uses
         Winapi.Windows, system.UIConsts, system.UITypes;
 
-    function colourToAlphaColour(   const colourIn  : TColor;
-                                    const opacityIn : integer = 255) : TAlphaColor;
+    //make colours from RGBA combinations
+        //TAlphaColor
+            function makeAlphaColour(   const redIn, greenIn, blueIn    : byte;
+                                        const opacityIn                 : byte = 255) : TAlphaColor; overload; inline;
 
-    function makeAlphaColour(redIn, greenIn, blueIn, opacityIn : byte) : TAlphaColor; inline;
+            function makeAlphaColour(const RGBQuadIn : TRGBQuad) : TAlphaColor; overload;
 
-    function makeColour(redIn, greenIn, blueIn : byte) : TColor;
+        //TColor
+            function makeColour(const redIn, greenIn, blueIn : byte) : TColor; overload;
+
+            function makeColour(const RGBQuadIn : TRGBQuad) : TColor; overload;
+
+    //convert a TColor to TAlphaColor
+        function colourToAlphaColour(   const colourIn  : TColor;
+                                        const opacityIn : byte = 255) : TAlphaColor;
 
 implementation
 
-    function colourToAlphaColour(   const colourIn  : TColor;
-                                    const opacityIn : integer = 255) : TAlphaColor;
-        var
-            colourReferenceHex  : LongInt;
-            redGreenBlue        : TRGBTriple;
-        begin
-            colourReferenceHex := TColorRec.ColorToRGB(colourIn);
+    //get alpha value
+        function getAlphaValue(const alphaColourIn : TAlphaColor) : byte;
+            var
+                colourReferenceHex : COLORREF;
+            begin
+                colourReferenceHex := TAlphaColorRec.ColorToRGB( alphaColourIn );
 
-            redGreenBlue.rgbtRed    := GetRValue( colourReferenceHex );
-            redGreenBlue.rgbtGreen  := GetGValue( colourReferenceHex );
-            redGreenBlue.rgbtBlue   := GetBValue( colourReferenceHex );
+                result := ( (colourReferenceHex shr 24) AND $FF );
+            end;
 
-            result := MakeColor( redGreenBlue.rgbtRed, redGreenBlue.rgbtGreen, redGreenBlue.rgbtBlue, opacityIn );
-        end;
+    //extract a colour's RGBA values
+        function extractColourRGB(const colourIn : Tcolor) : TRGBQuad;
+            var
+                colourReferenceHex  : COLORREF;
+                redGreenBlueOut     : TRGBQuad;
+            begin
+                colourReferenceHex := TColorRec.ColorToRGB( colourIn );
 
-    function makeAlphaColour(redIn, greenIn, blueIn, opacityIn : byte) : TAlphaColor;
-        begin
-            result := MakeColor( redIn, greenIn, blueIn, opacityIn );
-        end;
+                redGreenBlueOut.rgbRed      := GetRValue( colourReferenceHex );
+                redGreenBlueOut.rgbGreen    := GetGValue( colourReferenceHex );
+                redGreenBlueOut.rgbBlue     := GetBValue( colourReferenceHex );
+                redGreenBlueOut.rgbReserved := 255;
 
-    function makeColour(redIn, greenIn, blueIn : byte) : TColor;
-        var
-            tempAlphaColour : TAlphaColor;
-        begin
-            tempAlphaColour := MakeColor(redIn, greenIn, blueIn, 255);
+                result := redGreenBlueOut;
+            end;
 
-            result := AlphaColorToColor( tempAlphaColour );
-        end;
+        function extractAlphaColourRGBA(const alphaColourIn : TAlphaColor) : TRGBQuad;
+            var
+                tempColour              : TColor;
+                redGreenBlueAlphaOut    : TRGBQuad;
+            begin
+                tempColour := AlphaColorToColor( alphaColourIn );
+
+                redGreenBlueAlphaOut                := extractColourRGB( tempColour );
+                redGreenBlueAlphaOut.rgbReserved    := getAlphaValue( alphaColourIn );
+
+                result := redGreenBlueAlphaOut;
+            end;
+
+    //make colours from RGBA combinations
+        //TAlphaColor
+            function makeAlphaColour(   const redIn, greenIn, blueIn    : byte;
+                                        const opacityIn                 : byte = 255) : TAlphaColor;
+                begin
+                    result := MakeColor( redIn, greenIn, blueIn, opacityIn );
+                end;
+
+            function makeAlphaColour(const RGBQuadIn : TRGBQuad) : TAlphaColor;
+                begin
+                    result := makeAlphaColour( RGBQuadIn.rgbRed, RGBQuadIn.rgbGreen, RGBQuadIn.rgbBlue, RGBQuadIn.rgbReserved );
+                end;
+
+        //TColor
+            function makeColour(const redIn, greenIn, blueIn : byte) : TColor;
+                var
+                    tempAlphaColour : TAlphaColor;
+                begin
+                    tempAlphaColour := makeAlphaColour(redIn, greenIn, blueIn, 255);
+
+                    result := AlphaColorToColor( tempAlphaColour );
+                end;
+
+            function makeColour(const RGBQuadIn : TRGBQuad) : TColor;
+                begin
+                    result := makeColour( RGBQuadIn.rgbRed, RGBQuadIn.rgbGreen, RGBQuadIn.rgbBlue );
+                end;
+
+    //convert a TColor to TAlphaColor
+        function colourToAlphaColour(   const colourIn  : TColor;
+                                        const opacityIn : byte = 255) : TAlphaColor;
+            var
+                redGreenBlueAlpha : TRGBQuad;
+            begin
+                redGreenBlueAlpha := extractColourRGB( colourIn );
+
+                result := makeAlphaColour( redGreenBlueAlpha );
+            end;
 
 end.
