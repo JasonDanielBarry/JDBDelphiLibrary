6b479fa05d79c84d13dd13b45d744675ce0c1afd
Jason Barry <jasonbarry@prokon.com>
2024-11-26 23:33:59 +0200

    (1)

--- Source/Graphic2DTest.pas
+++ Source/Graphic2DTest.pas
@@ -307,8 +307,6 @@ implementation
     procedure TForm1.JDBGraphic2D1UpdateGeometry(   ASender : TObject;
                                                     var AGeomDrawer : TGeomDrawer);
         begin
-
-
             case (ComboBox1.ItemIndex) of
                 0:
                     BlueBoxGraphic( AGeomDrawer );
